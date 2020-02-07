6ba21d39c3eb6183e986b33ad4617dbee34b886b
abberihal <43981578+abberihal@users.noreply.github.com>
2020-02-07 17:43:55 +0100

    update

--- lessons.js
+++ lessons.js
@@ -28,7 +28,7 @@ function calcDiff(date1,date2){
      
 }
 function renderNextLessons(lesson, time, div){
-    //console.log(lesson.columns)
+    console.log(lesson.columns)
     if (lesson.columns[1] === 'Gem aktivitet' || lesson.columns[5] === "Annan aktivitet"){
         if (lesson.columns[4] != ""){
             div.getElementsByClassName('lessons')[0].innerText += lesson.columns[2] + " börjar om " + time + " i " + lesson.columns[4]+ '\n'
@@ -87,17 +87,22 @@ function currentLesson(lesson, startTime, currentDate, endTime, div){
 function nextLesson(lessons, div){
     var moderna = false
     lessons.forEach(element => {
-        
+        // console.log(element)
         var currentDate = today()
         var currentDay = currentDate.getDate()
+        var currentMonth = currentDate.getMonth()+1
         if (currentDate.getDate() < 10){
             currentDay = "0" + currentDay
         }
+        if (currentMonth < 10){
+            currentMonth = "0"+currentMonth
+        }
 
     
-        console.log(currentDate.getFullYear()+'-'+currentDate.getMonth()+'-'+ currentDay)
-        if (element.startdate === currentDate.getFullYear()+'-'+(currentDate.getMonth()+1)+'-'+ currentDay){
-            // console.log(element.columns[1])
+        console.log(currentDate.getFullYear()+'-'+currentMonth+'-'+ currentDay)
+        // console.log(element.startdate)
+        if (element.startdate === currentDate.getFullYear()+'-'+currentMonth+'-'+ currentDay){
+            console.log(element.columns[1])
             // console.log(element.starttime)
             const dateStringStart = element.startdate +" "+ element.starttime
             const dateStringEnd = element.enddate +" "+ element.endtime
@@ -183,28 +188,6 @@ function startTime() {
         clear()
         doEverything("180s", "https://cloud.timeedit.net/abbindustrigymnasium/web/public1/ri1Y7X3QQQfZY6QfZ5064405y7Y7.json")
         doEverything("190s", "https://cloud.timeedit.net/abbindustrigymnasium/web/public1/ri1Y7X3QQQfZY6QfZ5064205y7Y7.json")
-    
-        if (!cleaning()){
-            if (switchTimeEdit == false){
-                document.getElementById('180sFrame').style.display ="block"
-                document.getElementById('190sFrame').style.display ="none"
-                document.getElementById('timeEditInfo').innerText = '180s' 
-                switchTimeEdit = true
-            } 
-            else {
-                document.getElementById('180sFrame').style.display ="none"
-                document.getElementById('190sFrame').style.display ="block"
-                document.getElementById('timeEditInfo').innerText = '190s' 
-                switchTimeEdit = false
-            }
-            displayCleaning = false
-        }
-        else if (cleaning() === true && displayCleaning === false){
-            calculateCleaning()
-            console.log("städa")
-            
-        }
-
         
         
     }
